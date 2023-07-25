733e1e07c8fd65b28d8ca663a1245f34df7b7c7c
Malhotraabhi <107138079+Malhotraabhi@users.noreply.github.com>
2023-07-25 13:10:22 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://abhi22525:BsjlPsAimhxx1NX2@cluster0.xqlhxhh.mongodb.net/database?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.xqlhxhh.mongodb.net/database?retryWrites=true&w=majority')
